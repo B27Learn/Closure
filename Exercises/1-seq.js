4c39d70ba0f73e660e645e0bbdddc18e0cf54bc2
B27 <ilia.burdukovski@ya.ru>
2022-01-18 17:25:07 +0800

    work

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,20 @@
 'use strict';
 
-const seq = (f) => (g) => (x) => 0;
+const seq = (f) => {
+    const fnArr = [];
+    if (typeof f === "function") {
+        fnArr.push(f);
+        return g => {
+          if (typeof g === "function") {
+            
+          }
+          seq(g)
+        }; 
+    } 
+    if (typeof f === "number") {
+        
+    }
+    (g) => (x) => 0
+};
 
 module.exports = { seq };
