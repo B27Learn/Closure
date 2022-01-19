3e8c581cd93d10668685386ca272b6647ef070b9
B27 <ilia.burdukovski@ya.ru>
2022-01-19 11:38:17 +0800

    check

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,20 +1,18 @@
-'use strict';
+"use strict";
 
-const seq = (f) => {
-    const fnArr = [];
-    if (typeof f === "function") {
-        fnArr.push(f);
-        return g => {
-          if (typeof g === "function") {
-            
-          }
-          seq(g)
-        }; 
-    } 
-    if (typeof f === "number") {
-        
-    }
-    (g) => (x) => 0
-};
+const seq = f => n => typeof n === "function" ? seq(g => f(n(g))) : f(n);
 
+// const f1 = x => x + 7;
+// const f2 = x => x * 2;
+// const f = seq(f1)(f2);
+// const x = 0;
+// const y = f(x);
+// const expected = 7;
+// if (y !== expected) {
+//   const msg = `seq(${f1})(${f2})(${x})`;
+//   throw new Error(`${msg} === ${y} expected: ${expected}`);
+// }
+
+// f(n(g))
+//
 module.exports = { seq };
--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,13 @@
-'use strict';
+"use strict";
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const arrFn = i => {
+    return arr[i];
+  };
+  arrFn.push = value => arr.push(value);
+  arrFn.pop = () => arr.pop();
+  return arrFn;
+};
 
 module.exports = { array };
